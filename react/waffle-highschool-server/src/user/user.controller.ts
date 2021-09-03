48e05b1a1c26da3b517cc877cb49d729c23e88f3
woohm402 <woohm404@gmail.com>
2021-09-03 18:35:56 +0900

    서버 레포 분리

--- react/waffle-highschool-server/src/user/user.controller.ts
+++
@@ -1,7 +0,0 @@
-import { UserService } from './user.service';
-import { Controller } from '@nestjs/common';
-
-@Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-}
